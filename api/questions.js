c0dbab4080d02972ddc6109dfd46667782e391b0
sandeepkumare <Sandeepkumar.E@pommala.com>
2025-05-14 21:48:42 +0530

    questions are updated.

--- api/questions.js
+++ api/questions.js
@@ -14,18 +14,21 @@ export default async function handler(req, res) {
   try {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-
     const filePath = path.join(__dirname, 'questions.json');
     const file = await fs.readFile(filePath, 'utf-8');
     const data = JSON.parse(file);
 
     const { level, technology, topic } = req.query;
-    const filtered = data.filter(
-      q =>
+
+    let filtered = data;
+
+    if (level && technology && topic) {
+      filtered = data.filter(q =>
         q.level === level &&
         q.technology === technology &&
         q.topic === topic
-    );
+      );
+    }
 
     res.status(200).json(filtered);
   } catch (error) {
